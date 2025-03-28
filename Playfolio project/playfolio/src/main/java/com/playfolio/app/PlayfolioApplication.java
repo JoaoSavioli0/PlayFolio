a05ea814614fbde0a0401165c44fc4a61bc0288c
JoaoSavioli21 <105456605+JoaoSavioli0@users.noreply.github.com>
2025-03-28 09:35:51 -0300

    Primeiro commit

---
+++ Playfolio project/playfolio/src/main/java/com/playfolio/app/PlayfolioApplication.java
@@ -0,0 +1,13 @@
+package com.playfolio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayfolioApplication.class, args);
+	}
+
+}
